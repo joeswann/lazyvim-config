692c9529ad3d5d6e6381de2dbb4d5f216e0e345a
Joe Swann <joe@1of1studio.com>
2024-05-18 11:53:59 +1200

    chore(email.lua): comment out Himalaya plugin configuration to disable it temporarily

--- lua/plugins/email.lua
+++ lua/plugins/email.lua
@@ -1,9 +1,9 @@
 return {
-  {
-    "https://git.sr.ht/~soywod/himalaya-vim",
-    config = function()
-      -- vim.g.himalaya_executable = "/opt/homebrew/bin/himalaya"
-      -- vim.g.himalaya_folder_picker = "telescope"
-    end,
-  },
+  -- {
+  --   "https://git.sr.ht/~soywod/himalaya-vim",
+  --   config = function()
+  --     -- vim.g.himalaya_executable = "/opt/homebrew/bin/himalaya"
+  --     -- vim.g.himalaya_folder_picker = "telescope"
+  --   end,
+  -- },
 }
